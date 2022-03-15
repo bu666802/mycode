45bab5fec4a505fdee9076f1694459c0401735ee
bu666802 <35322367+bu666802@users.noreply.github.com>
2022-03-15 19:59:48 +0800

    Add files via upload

--- UploadAction.java
+++ UploadAction.java
@@ -4,6 +4,8 @@ package com.web.upload;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -12,68 +14,119 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.SystemConfig;
+import com.SystemProperties;
 
 
-@WebServlet(name="uploadAction", urlPatterns={"/uploads/*"}, loadOnStartup=1)
+
+@WebServlet(urlPatterns={"/upload/*"},loadOnStartup=1)
 public class UploadAction extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	
-	private Logger logger = Logger.getLogger(this.getClass());
 
-	private String savePath = SystemConfig.getInstance().getProperty("uploadSavePath"); 
+	private File   uploadSaveFile ;   
+	
+	
+	public UploadAction(){
+				
+		//可将路径放置设定档
+		
+		String systemFilePath = SystemProperties.getInstance().getProperty("systemFilePath");
+		File rootFile  = new File(systemFilePath);
+		//File  ftpFile  = new File(rootFile,"ftp");
+		uploadSaveFile = rootFile;
+   
+		
+	}
 	
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException {
     	
     	
-    	String uri = req.getRequestURI().substring(1);
-    	String mapPath= uri.substring(uri.indexOf("/uploads/")+1, uri.length());
-    	File    saveFile = new  File(savePath,mapPath);
-    	logger.debug("[uploadAction][URL ]"+req.getRequestURL());
-    	logger.debug("[uploadAction][Disk]"+saveFile.getAbsolutePath());   	
-    	if(saveFile.exists()){
+    	//String uri         = req.getRequestURI();
+    	
+    	String uri = URLDecoder.decode(req.getRequestURI(), "UTF-8"); 	
+    	String uriPath     = uri.substring(uri.indexOf("upload/"), uri.length() );
+    	System.out.println(uriPath);
+    	File   saveFile    = new  File(uploadSaveFile,uriPath);
+    	
+    	System.out.println(saveFile.getAbsolutePath());
+    	
+    	if(saveFile.exists()) {
     		writeFile(res,saveFile);
-    	}	
-	
+    	}else {
+    		
+    		System.out.println(saveFile.getCanonicalPath());
+    		System.out.println("文件不存在" );
+    	}
     }
+    
+    
     
     void writeFile(  HttpServletResponse res ,File file)throws ServletException, IOException {
-        
-    	java.util.Date date = new java.util.Date();  
-		res.setDateHeader("Last-Modified",date.getTime());
-		res.setDateHeader("expires",System.currentTimeMillis()+60*60*24*30);
     	
-    	ServletOutputStream sout=res.getOutputStream();
-		FileInputStream  in = new FileInputStream(file);  
-		byte b[]=new byte[4096];
-        while(in.read(b)!=-1){
-            sout.write(b);
-           
-        } 
-       
-       in.close();
-       sout.flush();
-       sout.close();
     	
-    }
+    	if(file.getName().endsWith("xlsx")) {
     	
-		
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
-        
-    	doPost(req ,resp );
-    	
-    }
-    
-    
+    		res.setContentType("application/vnd.ms-excel");
+    		res.setCharacterEncoding("UTF-8");
+    		res.setHeader("Content-Disposition", "attachment; filename="+file.getName());
+    		System.out.println("download excel..");
+        	ServletOutputStream out=res.getOutputStream();
+    		
+        	Workbook workbook;
+			try {
+				workbook = new XSSFWorkbook(file);
+				res.setContentType("application/vnd.ms-excel");
+	        	//res.setHeader("Content-Disposition", "attachment; filename=YourFilename.xlsx");
+	        	ServletOutputStream outputStream = res.getOutputStream();
+	        	workbook.write(outputStream);
+	            out.flush();
+	            out.close();
+				
+			} catch (InvalidFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
+        	
+    		
+    		
+    	}else {
+    		Date date = new Date();  
+        	res.setContentType("image/*");
+        	res.setDateHeader("Last-Modified",date.getTime());
+    		res.setDateHeader("expires",System.currentTimeMillis()+9999999);
+    		res.setCharacterEncoding("UTF-8");
+    		
+    		
+    		
+        	ServletOutputStream out=res.getOutputStream();
+    		FileInputStream  in = new FileInputStream(file);  
+    		byte b[]=new byte[4096];
+            while(in.read(b)!=-1){
+                out.write(b);
+            }  
+            in.close();
+            out.flush();
+            out.close();
+    		
+    	}
+    	
+    
+		
+		
+		
+	
+    	
+    }
     
   
-    
+
+
 }
 
 
--- UploadFile.java
+++ UploadFile.java
@@ -1,58 +1,65 @@
 package com.web.upload;
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.URLDecoder;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.log4j.Logger;
 
 import com.CheckUtil;
 import com.JSONListFormat;
-import com.SystemConfig;
+import com.SystemProperties;
 import com.web.WebUtil;
 
-import it.sauronsoftware.jave.AudioAttributes;
-import it.sauronsoftware.jave.Encoder;
-import it.sauronsoftware.jave.EncoderException;
-import it.sauronsoftware.jave.EncodingAttributes;
-import it.sauronsoftware.jave.InputFormatException;
 
-@WebServlet(name="uploadFile", urlPatterns={"/uploadFile"}, loadOnStartup=1)
+@WebServlet("/uploadFile")
+@MultipartConfig 
 public class UploadFile extends HttpServlet {
 	
 	
 	private Logger logger = Logger.getLogger(this.getClass());
 	
 	
-	
 	private static final long serialVersionUID = 1L;
-	private static final int MEMORY_THRESHOLD = 1024 * 1024 * 3;      // 3MB
-	private static final int MAX_FILE_SIZE    = 1024 * 1024 * 40;     // 40MB
-	private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 1350;   // 150MB
 	
 	
+	private SimpleDateFormat nameDateFormat    = new SimpleDateFormat("yyyyMMddHHmmss");
+	private SimpleDateFormat dateSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 	
-	private SimpleDateFormat simpleDateFormat    = new SimpleDateFormat("HHmm");
-	private SimpleDateFormat nowSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	
+	private String systemFilePath     = SystemProperties.getInstance().getProperty("systemFilePath"); 
+	private String serverURL          = SystemProperties.getInstance().getProperty("serverURL"); 
+	private File   uploadSaveFile    = new  File(systemFilePath);
+	
+	
 	public UploadFile(){
 		
-		System.out.println("== uploadFile start  ===");
+		
+		logger.info("===========================");
+		logger.info("  文件上传接口（2） web启动成功");
+		logger.info("===========================");
+			
+		logger.info("[uploadFile][systemFilePath ]"+systemFilePath);
+
+		
 	}
 
     @Override
@@ -61,143 +68,107 @@ public class UploadFile extends HttpServlet {
     	res.setCharacterEncoding("UTF-8");
 		req.setCharacterEncoding("UTF-8");
 		String responseMessage = "";
-		
-		String path = req.getParameter("path");
-		
-		
-		
-		if(CheckUtil.isEmpty(path))path= nowSimpleDateFormat.format(new Date());
-	
-		path = URLDecoder.decode(path,"utf-8");
-		path= path.replaceAll("\\.", "");
-		
-		
-		System.out.println("###### start upload ######");
-		
-		
-		
+
 		JSONListFormat jsonListFormat  = WebUtil.createJSONListFormat(req, false);
+		String path = req.getParameter("path");
+		if(CheckUtil.isEmpty(path))path="test";
 		
+		System.out.println();  
+		
+		String contentType = req.getContentType();
 	
+		System.err.println("[req.contentTypes]"+contentType);
 		
-		if (!ServletFileUpload.isMultipartContent(req)) {
-			
-			responseMessage="error-noMultipartContent";
-		}
-		
-		
-		
-		
-		if(responseMessage == ""){
-			
-			
-			
-			
-			
-			try {
-				
-				
-				DiskFileItemFactory factory = new DiskFileItemFactory();
-				factory.setSizeThreshold(MEMORY_THRESHOLD);
-				factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
-
-				ServletFileUpload upload = new ServletFileUpload(factory);
-				upload.setFileSizeMax(MAX_FILE_SIZE);
-				upload.setSizeMax( MAX_REQUEST_SIZE);
-				
-				
-				List<FileItem> formItems =   upload.parseRequest(req);
-				
-				
-				if (formItems != null && formItems.size() > 0) {
-					
-					
-					for (FileItem item : formItems) {
-						String fileName = item.getName();
-						
-						System.out.println("fileName="+fileName);
-						
-						if (item.isFormField()) {
-							
-							System.out.println("普通输入项的数据");
-					
-						}else{
-							
-						
-							fileName = fileName.toLowerCase();
-							String subFileName ="";
-							String[] uploadFormats = SystemConfig.getInstance().getProperty("uploadFormat").split("\\|");
-							for (String string : uploadFormats) {
-								if(fileName.endsWith(string)) subFileName = string;
-							}
-							if(subFileName=="") responseMessage="error-format";     
-						
-							
-							
-							if(responseMessage ==""){
-								
-								String uploadServerURL = SystemConfig.getInstance().getProperty("uploadServerURL");
-								String uploadSavePath  = SystemConfig.getInstance().getProperty("uploadSavePath");
-							
-								
-								String uploadPath      =  "uploads/"+path+"/"+nowSimpleDateFormat.format(new Date());
-								File   uploadFoldFile  = new File(uploadSavePath,uploadPath  );
-								
-								if(!uploadFoldFile.exists())uploadFoldFile.mkdirs();
-								
-								
-								
-								String name =  simpleDateFormat.format(new Date()) + UUID.randomUUID().toString().substring(0, 8);
-							
-								File storeFile = new File(uploadFoldFile ,   name+ "."+subFileName);
-								
-								item.write(storeFile);
-								System.out.println("[save]-->"+ storeFile.getAbsoluteFile());
-								
-								
-//								if("amr".equals(subFileName)){
-//									
-//									File storeFile2= new File(uploadFoldFile , name +".mp3" );
-//									changeToMp3(storeFile,storeFile2 );
-//									storeFile = storeFile2;
-//									subFileName = "mp3";
-//									
-//								}
-							
-								String urlPath =  uploadPath+"/"+ name+ "."+subFileName;
-								
-							
-								HashMap<String,String> map =  new HashMap<String,String>();
-								map.put("fileURL", uploadServerURL+urlPath);
-								map.put("fileURI", urlPath);
-								jsonListFormat.addMap(map);
-								
-								
-							}
-							
-						
-							
-						}
-					}
-					
-				}else{
-					responseMessage ="error-fileEmpty";
-				}
-			
-				
-				
-				
-			} catch (Exception e) {
-				responseMessage = e.getMessage();
-				logger.error(e,e);
-				
+		      
+        if(!req.getContentType().split(";")[0].equals("multipart/form-data"))  return;  
+        //print info
+        
+        Collection<Part> parts = req.getParts();
+        int index=0;
+        for (Part part : parts) {
+        	index++;
+        	System.out.println("======================== part"+index+"========================");
+        	System.out.println("[part_Name]"+part.getName());
+        	System.out.println("[part_ContentType]"+part.getContentType());
+        	System.out.println("[part_Size]"+part.getSize());
+        	Collection<String> names = part.getHeaderNames();
+        	for (String string : names) {
+        		System.out.println("["+string+"]"+part.getHeader(string));
 			}
-		
-			
-
-			
-			
-		}
+        }
+        System.out.println();
+        
+        
+      
+        Part namePart = req.getPart("name");
+        if(namePart!=null) {
+        	InputStream inputStream = namePart.getInputStream();
+        	StringBuilder sb = new StringBuilder();
+        	String line;
+        	BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        	while ((line = br.readLine()) != null) {
+        	    sb.append(line);
+        	}
+        	String str = sb.toString();
+        	System.err.println("name:"+str);
+        	System.out.println();
+        }
+        
+        
+        
+        Part filePart = req.getPart("file");
+    	if(filePart==null)responseMessage="error-file";
+    	if(responseMessage==""){
+    		logger.info("       name = "+filePart.getName());
+  	      	logger.info("   FileName = "+filePart.getSubmittedFileName()); 
+  	      	logger.info("       Size = "+filePart.getSize()); 
+  	      	logger.info("ContentType = "+filePart.getContentType());
+  	      
+  	      try {
+  	    	  
+  	    	  
+  	    	  String    fileName = filePart.getSubmittedFileName();
+  	    	  String lowFileName = fileName.toLowerCase();
+  	    	  String subFileName = "";
+  	    	  String[] uploadFormats = SystemProperties.getInstance().getProperty("uploadFormat").split("\\|");
+ 			
+  	    	  for (String string : uploadFormats) {
+  	    		  if(lowFileName.endsWith(string)) subFileName = string;
+  	    	  }
+  	    	  if(subFileName=="") responseMessage="error-format";
+        	
+  	    	  
+  	    	  String uuid = UUID.randomUUID().toString().replaceAll("-", "").toLowerCase().substring(20);
+  	    	  
+  	    	  if(responseMessage==""){
+  	    		  String newName          = nameDateFormat.format(new Date()) + uuid +"."+subFileName ;
+  	    		  String uriPath  = "upload/"+ path+"/"+dateSimpleDateFormat.format(new Date())  +"/" +newName   ;
+  	    		  File  storeFile = new File(uploadSaveFile ,uriPath);
+  	    		  storeFile.getParentFile().mkdirs();
+  	    		  filePart.write(storeFile.getAbsolutePath());
+             	
+  	    		  logger.info("[uploadFile]     uriPath-->"+uriPath);
+  	    		  logger.info("[uploadFile] 文件保存路经-->"+storeFile.getAbsolutePath());
+             	
+  	    		  HashMap<String,String> map =  new HashMap<String,String>();
+  	    		
+  	    		  map.put("serverURL", serverURL);
+  	    		  map.put(  "fileURI", uriPath);
+  	    		  map.put( "filePath", storeFile.getAbsolutePath());
+  	    		  map.put( "fileSize", String.valueOf(storeFile.length()));
+  	    		  map.put( "fileName", filePart.getSubmittedFileName());
+  	    		  jsonListFormat.addObject(map);
+	 				
+  	    	  }   	
+  	    	  
+  	      } catch (Exception e) {
+  	    	  responseMessage = e.getClass().getName();
+  	    	  logger.error(e,e);
+  	      }
+    	}
+      
+        
+    
 
 		
 		if(responseMessage=="")responseMessage="success";
@@ -209,29 +180,6 @@ public class UploadFile extends HttpServlet {
 			
 
 	}
-    
-    public  void changeToMp3( File source,  File target) {  
-        
-        AudioAttributes audio = new AudioAttributes();  
-        Encoder encoder = new Encoder();  
-  
-        audio.setCodec("libmp3lame");  
-        EncodingAttributes attrs = new EncodingAttributes();  
-        attrs.setFormat("mp3");  
-        attrs.setAudioAttributes(audio);  
-  
-        try {  
-            encoder.encode(source, target, attrs);  
-        }catch (InputFormatException e) {  
-        	  System.out.println("2:"+e);  
-        }catch (EncoderException e) {  
-        
-        }catch (Exception e) {  
-        	throw e;
-        } 
-    } 
-    
-    
 
 	
 }
